3d3c2c5bd380fcb4ae69cd07f2026e3f3f3ea256
Maleva Olga <formaleva@gmail.com>
2022-01-25 22:38:32 +0300

    add bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,13 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: process.env.NODE_ENV || "development",
-  plugins: [new HtmlWebpackPlugin({ template: "template.html" })]
+  plugins: [new HtmlWebpackPlugin({ template: "template.html" })],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  }
 };
