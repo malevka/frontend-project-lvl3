ae80691c798204d37e4486600983e90eca87919f
Maleva Olga <formaleva@gmail.com>
2022-05-27 11:29:57 +0300

    update package.json, webpack.config, postcss.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 const devMode = process.env.NODE_ENV !== "production";
 
-module.exports = {
+export default {
   mode: process.env.NODE_ENV || "development",
   module: {
     rules: [
