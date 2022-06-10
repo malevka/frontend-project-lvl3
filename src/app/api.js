367ee5077fb1d508a20763b659dea72a1f537697
Maleva Olga <formaleva@gmail.com>
2022-06-10 19:44:24 +0300

    remove waste chaeking of respose code

--- src/app/api.js
+++ src/app/api.js
@@ -1,7 +1,4 @@
 import axios from "axios";
 
 const proxy = "https://allorigins.hexlet.app/get?disableCache=true&url=";
-export default (url) => axios
-  .get(`${proxy}${encodeURIComponent(url)}`)
-  .then((result) => (result.data.status.http_code !== 200
-    ? Promise.reject() : result.data.contents));
+export default (url) => axios.get(`${proxy}${encodeURIComponent(url)}`).then((result) => result.data.contents);
